ed84672143b151b6b81083a7e316988b866115e8
qlql765 <108382776+qlql765@users.noreply.github.com>
2022-08-23 10:05:40 +0800

    内置接口

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -56,7 +56,7 @@ public class ApiDialog extends BaseDialog {
         tvAddress = findViewById(R.id.tvAddress);
         inputApi = findViewById(R.id.input);
         //内置网络接口在此处添加
-        inputApi.setText(Hawk.get(HawkConfig.API_URL, ""));
+        inputApi.setText(Hawk.get(HawkConfig.API_URL, "https://gitcode.net/qlql765/q/-/raw/master/ql6.json"));
         findViewById(R.id.inputSubmit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
